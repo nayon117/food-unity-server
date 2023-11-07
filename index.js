b25a9c3fb16dbd379fc12a291a11acee83e00c78
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-07 21:20:26 +0600

    implement delete opearation in foods collection

--- index.js
+++ index.js
@@ -118,6 +118,15 @@ async function run() {
       res.send(result);
     });
 
+    // delete method for foods
+    app.delete("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await foodCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
